57c911b3b322b5a4c296832bd874442ed2d1e34d
Joan <joan.rivas.cepeda@gmail.com>
2026-01-22 03:13:02 -0300

    error on override

--- src/main/java/com/baoh/power/BaohPowerImpl.java
+++ src/main/java/com/baoh/power/BaohPowerImpl.java
@@ -1,18 +1,45 @@
 package com.baoh.power;
 
 import com.baoh.power.skill.BaohSkill;
+import com.github.standobyte.jojo.power.impl.nonstand.INonStandPower;
+import com.github.standobyte.jojo.power.impl.nonstand.TypeSpecificData;
+import com.github.standobyte.jojo.power.impl.nonstand.type.NonStandPowerType;
 
 import java.util.EnumMap;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 
-public class BaohPowerImpl implements IBaohPower {
+public class BaohPowerImpl extends TypeSpecificData implements IBaohPower {
     private final Map<BaohStat, Integer> statLevels = new EnumMap<>(BaohStat.class);
+    private final Set<BaohSkill> unlockedSkills = new HashSet<>();
     private int getAvailablePoints;
+    private float energy = 0;
+    private boolean hadPowerBefore = false;
 
     public BaohPowerImpl() {
         for (BaohStat stat : BaohStat.values()) {
             statLevels.put(stat, 0);
         }
+    }
+
+    @Override
+    @SuppressWarnings("unchecked")
+    public <T extends com.github.standobyte.jojo.power.impl.nonstand.TypeSpecificData> T getTypeSpecificData(NonStandPowerType<T> powerType)
+    {
+        return (T) this;
+    }
+
+    @Override
+    public boolean hadPowerBefore(NonStandPowerType<?> powerType)
+    {
+        return hadPowerBefore;
+    }
+
+    @Override
+    public void addHadPowerBefore(NonStandPowerType<?> powerType)
+    {
+        this.hadPowerBefore = true;
     }
 
     @Override
@@ -49,12 +76,74 @@ public class BaohPowerImpl implements IBaohPower {
 
     }
 
-//    @Override
-//    public boolean isSkillUnlocked(BaohSkill skill)
-//    {
-//        return unlo
-//    }
-  //  void unlockSkill(BaohSkill skill);
+    @Override
+    public boolean isSkillUnlocked(BaohSkill skill)
+    {
+        return unlockedSkills.contains(skill);
+    }
+
+    @Override
+    public void unlockSkill(BaohSkill skill)
+    {
+        this.unlockedSkills.add(skill);
+    }
+
+    @Override
+    public float getEnergy()
+    {
+        return this.energy;
+    }
+
+    @Override
+    public void setEnergy(float energy)
+    {
+        this.energy = Math.max(0, Math.min(energy, getMaxEnergy()));
+    }
+
+    @Override
+    public float getMaxEnergy()
+    {
+        return 100.0f;
+    }
+
+    @Override
+    public boolean consumeEnergy(float amount)
+    {
+        if (this.getEnergy() <= 0.0f)
+        {
+            return false;
+        }
+
+        int will = getStatLevel(BaohStat.WILL);
+        int willCap = Math.min(will, 100);
+
+        float reductionFactor = 1.0f - (willCap * 0.008f);
+
+        float subtractAmount = amount * reductionFactor;
+
+        this.setEnergy(this.getEnergy() - subtractAmount);
+
+        return true;
+    }
+
+    @Override
+    public boolean hasEnergy(float amount)
+    {
+        return this.getEnergy() >= amount;
+    }
+
+    @Override
+    public void addEnergy(float amount)
+    {
+        int mind = getStatLevel(BaohStat.MIND);
+        int mindCap = Math.min(mind, 100);
+
+        float bonusAmount = 1.0f + (mindCap * 0.008f);
+
+        float gainAmount = amount * bonusAmount;
+
+        this.setEnergy(this.getEnergy() + gainAmount);
+    }
 
     private void checkUnlockSkill()
     {
