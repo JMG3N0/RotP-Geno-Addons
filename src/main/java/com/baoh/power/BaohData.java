2ee4424f92562c225204bc9eb512079f4097f1f1
Joan <joan.rivas.cepeda@gmail.com>
2026-02-01 20:45:47 -0300

    started fixing powertype

--- src/main/java/com/baoh/power/BaohData.java
+++ src/main/java/com/baoh/power/BaohData.java
@@ -8,33 +8,36 @@ import net.minecraftforge.fml.common.Mod;
 
 public class BaohData extends TypeSpecificData {
 
-    public static final int MAX_STAT_LEVEL = 100;
+    public int maxStatLevel = 100;
+    public int maxLevel = 100;
+    private int currentLevel = 0;
+    private final int daysToLive = 110;
+
+    public float maxAdrenaline = 100.0F;
+    private float currentAdrenaline = 0.0F;
+
+    private int strPoints = 0;
+    private int strLevel = 0;
+    private int dexPoints = 0;
+    private int dexLevel = 0;
+    private int willPoints = 0;
+    private int willLevel = 0;
+    private int conPoints = 0;
+    private int conLevel = 0;
+    private int mindPoints = 0;
+    private int mindLevel = 0;
 
 
 
-    private int strengthPoints;
-    private int strengthLevel;
-    private int agilityPoints;
-    private int agilityLevel;
-    private int willPoints;
-    private int willLevel;
-    private int endurancePoints;
-    private int enduranceLevel;
-    private int mindPoints;
-    private int mindLevel;
-    private int totalLevel;
-    private int adrenalineAmount;
-    private int daysAlive;
 
-    private float damageFactor = 1F;
-
-    private boolean isTransformed;
-    private boolean isSuspended;
-    private boolean isUnified;
-    private boolean saberOut;
-    private boolean knowsHappiness;
-    private boolean knowsSadness;
-    private boolean knowsAnger;
-    private boolean knowsFear;
-
+    private boolean isTransformed = false;
+    private boolean isSuspended = false;
+    private boolean isUnified = false;
+    private boolean saberOut = false;
+    private boolean knowsHappiness = false;
+    private boolean knowsSadness = false;
+    private boolean knowsAnger = false;
+    private boolean knowsFear = false;
+    private boolean knowsCourage = false;
+    private boolean knowsMalice = false;
 }
